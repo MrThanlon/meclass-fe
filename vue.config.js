6e3d2b4d95b78eb7e99cc15816786c10d13634eb
MrThanlon <i@anclx.cc>
2020-06-29 15:15:13 +0800

    add: dev server
    Signed-off-by: MrThanlon <i@anclx.cc>

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   publicPath: '',
-  productionSourceMap: false
+  productionSourceMap: false,
+  devServer: {
+    proxy: 'http://47.103.204.9:8080/meclass-0.0.1-SNAPSHOT'
+  }
 }
